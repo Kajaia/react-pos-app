5bdb0a0248a9d7693fbb1e6efc7fcd14c9c93a3c
Kajaia <lashakajaia31@gmail.com>
2022-10-25 19:30:02 +0400

    Added options

--- src/features/cart/CartTotal.jsx
+++ src/features/cart/CartTotal.jsx
@@ -1,16 +1,73 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { resetCart } from "./cartSlice";
 
 export default function CartTotal({ total }) {
+  const [options, setOptions] = useState({
+    tax: 10,
+    discount: 0,
+    shipping: 2.99,
+  });
   const dispatch = useDispatch();
+  const tax = (total / 100) * options.tax;
+  const discount = (total / 100) * options.discount;
+  const shipping = Number(options.shipping);
+  const finalPrice = total + tax - discount + shipping;
 
   const resetCartItems = () => {
     dispatch(resetCart());
   };
 
+  const handleChange = (e) => {
+    setOptions((option) => {
+      return { ...option, [e.target.name]: e.target.value };
+    });
+  };
+
   return (
     <div className="card-footer mt-auto bg-white rounded-3 border-0 py-3">
-      <span className="badge bg-primary rounded-pill shadow-sm w-100 fs-5">{`Total price: $${total.toFixed(
+      <div className="mb-3">
+        <div className="row justify-content-center g-3">
+          <div className="col-6 col-md-4">
+            <label htmlFor="tax">Tax %</label>
+            <input
+              type="number"
+              name="tax"
+              id="tax"
+              value={options.tax}
+              onChange={(e) => handleChange(e)}
+              min="0"
+              className="form-control rounded-pill"
+            />
+          </div>
+          <div className="col-6 col-md-4">
+            <label htmlFor="discount">Discount %</label>
+            <input
+              type="number"
+              name="discount"
+              id="discount"
+              value={options.discount}
+              onChange={(e) => handleChange(e)}
+              min="0"
+              className="form-control rounded-pill"
+            />
+          </div>
+          <div className="col-6 col-md-4">
+            <label htmlFor="shipping">Shipping $</label>
+            <input
+              type="number"
+              name="shipping"
+              id="shipping"
+              value={options.shipping}
+              onChange={(e) => handleChange(e)}
+              min=".01"
+              step=".01"
+              className="form-control rounded-pill"
+            />
+          </div>
+        </div>
+      </div>
+      <span className="badge bg-primary rounded-pill shadow-sm w-100 fs-5">{`Total price: $${finalPrice.toFixed(
         2
       )}`}</span>
       <div className="mt-2 d-flex align-items-center justify-content-center flex-wrap gap-2">
